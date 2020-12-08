8fdd3f73b33fd0b0d704910e5136ec19c8a58513
aaclan <aaclan@ebi.ac.uk>
2020-12-08 14:26:01 +0000

    Check if validation errors is empty or undefined

--- src/listener/handlers/document-update-handler.ts
+++ src/listener/handlers/document-update-handler.ts
@@ -133,6 +133,10 @@ class DocumentUpdateHandler implements IHandler {
                     const err = new ErrorReport(ErrorType.FileNotUploaded, msg);
                     err.userFriendlyMessage = "File not uploaded.";
 
+                    if (!contentValidationReport.validationErrors || contentValidationReport.validationErrors.length == 0) {
+                        contentValidationReport.validationErrors = [];
+                    }
+
                     contentValidationReport.validationErrors.push(err)
                     return Promise.resolve(contentValidationReport);
                 }
