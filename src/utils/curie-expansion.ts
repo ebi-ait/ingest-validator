f64a7df6a95601ba1ab58fb8fdc4718313a84052
Amnon Khen <amnon@ebi.ac.uk>
2024-08-20 16:57:22 +0100

    dcp-963: upgrade to ols4 in dev environment

--- src/utils/curie-expansion.ts
+++ src/utils/curie-expansion.ts
@@ -1,33 +1,40 @@
 import config from "config";
 import request from "request-promise";
 import Promise from "bluebird";
+import * as QueryString from "querystring";
 
-const olsConnectionConfig :any = config.get("OLS_API.connection");
-const olsSearchUrl: string = olsConnectionConfig["url"] + "/api/search?q=";
+const olsConnectionConfig: any = config.get("OLS_API.connection");
+const olsSearchUrl: string = olsConnectionConfig["url"] + "/api/search";
 
 class CurieExpansion {
     cachedOlsCurieResponses: any = {};
-    constructor(){
+
+    constructor() {
         this.cachedOlsCurieResponses = {};
     }
 
     static isCurie(term: string) {
         let curie = true;
-        if (term.split(":").length != 2 || term.includes("http")){
+        if (term.split(":").length != 2 || term.includes("http")) {
             curie = false;
         }
         return curie;
     }
 
     expandCurie(term: string) {
-        const termUri = encodeURIComponent(term);
-        const url = olsSearchUrl + termUri
-            + "&exact=true&groupField=true&queryFields=obo_id";
-
+        const olsQueryString = QueryString.stringify(
+            {
+                q: term,
+                exact: true,
+                groupField: true,
+                queryFields: 'obo_id'
+            }
+        )
+        const url = olsSearchUrl + '?' + olsQueryString;
         return new Promise((resolve, reject) => {
-            let curieExpandResponsePromise: Promise<any>|null = null;
+            let curieExpandResponsePromise: Promise<any> | null = null;
 
-            if(this.cachedOlsCurieResponses[url]) {
+            if (this.cachedOlsCurieResponses[url]) {
                 curieExpandResponsePromise = Promise.resolve(this.cachedOlsCurieResponses[url]);
             } else {
                 curieExpandResponsePromise = request({
@@ -43,8 +50,7 @@ class CurieExpansion {
                     let jsonBody = resp;
                     if (jsonBody.response.numFound === 1) {
                         resolve(jsonBody.response.docs[0].iri);
-                    }
-                    else {
+                    } else {
                         reject(`Could not retrieve IRI for ${term}. OLS URL: ${url}`);
                     }
                 }).catch(err => {
